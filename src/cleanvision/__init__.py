9d3d476a8e3e56d2f96a8b5d22672815058adc26
Sanjana Garg <garg.sanjana.95@gmail.com>
2024-03-04 16:57:15 -0800

    Updated get version method

--- src/cleanvision/__init__.py
+++ src/cleanvision/__init__.py
@@ -1,3 +1,4 @@
+import importlib.metadata
 import sys
 from typing import Any, Union
 
@@ -7,19 +8,8 @@ PYTHON_VERSION_INFO = sys.version_info
 
 
 def get_version() -> Union[str, Any]:
-    if sys.version_info.major >= 3 and sys.version_info.minor >= 8:
-        import importlib.metadata
-
-        return importlib.metadata.version("cleanvision")
-    else:
-        import importlib_metadata
-
-        return importlib_metadata.version("cleanvision")
+    return importlib.metadata.version("cleanvision")
 
 
-try:
-    __version__ = get_version()
-except Exception:
-    pass
-
+__version__ = get_version()
 Imagelab = _Imagelab
